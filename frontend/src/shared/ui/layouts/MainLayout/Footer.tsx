10a54d12f6c7b74643eb00ebaafeb9ebc6d4b3c3
PulsarBITW <GordeyBITW@gmail.com>
2024-12-24 01:22:55 +0300

    add prefix to css variables, fix footer text color

--- frontend/src/shared/ui/layouts/MainLayout/Footer.tsx
+++ frontend/src/shared/ui/layouts/MainLayout/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
-    <footer className="min-h-[48px] w-full bg-foreground">
-      <div className="text-footer-text mx-auto h-full max-w-[1200px]">Footer</div>
+    <footer className="min-h-[48px] w-full bg-footer">
+      <div className="mx-auto h-full max-w-[1200px] text-text-footer">Footer</div>
     </footer>
   );
 };
