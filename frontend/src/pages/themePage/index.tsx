db14734d54ddff798a60195fd367c6c18048d396
PulsarBITW <GordeyBITW@gmail.com>
2024-12-27 22:49:57 +0300

    update Button

--- frontend/src/pages/themePage/index.tsx
+++ frontend/src/pages/themePage/index.tsx
@@ -1,6 +1,5 @@
 import {Button, Papper} from '@shared/ui';
 import {Group} from './ui/group';
-import {ButtonV2} from '@shared/ui/buttonV2';
 
 export const ThemePage = () => {
   return (
@@ -9,143 +8,118 @@ export const ThemePage = () => {
         <h1 className="font-bold text-text-primary">Theme Page</h1>
       </div>
       <Group title="Buttons">
-        <Button>Primary</Button>
-        <Button disabled>Primary Disabled</Button>
-        <Button variant="secondary">secondary</Button>
-        <Button disabled variant="secondary">
-          Secondary Disabled
-        </Button>
-        <Button variant="success">Success</Button>
-        <Button disabled variant="success">
-          Success Disabled
-        </Button>
-        <Button variant="destructive">Destructive</Button>
-        <Button disabled variant="destructive">
-          Destructive Disabled
-        </Button>
-        <Button variant="outline">Outline</Button>
-        <Button disabled variant="outline">
-          Outline Disabled
-        </Button>
-        <Button variant="ghost">Ghost</Button>
-        <Button disabled variant="ghost">
-          Ghost Disabled
-        </Button>
-      </Group>
-
-      <Group title="Buttons V2">
         <Group title="Primary">
-          <ButtonV2 variant="fill" color="primary">
+          <Button variant="fill" color="primary">
             Fill
-          </ButtonV2>
-          <ButtonV2 variant="fill" color="primary" disabled>
+          </Button>
+          <Button variant="fill" color="primary" disabled>
             Fill disabled
-          </ButtonV2>
-          <ButtonV2 variant="fill-light" color="primary">
+          </Button>
+          <Button variant="fill-light" color="primary">
             fill-light
-          </ButtonV2>
-          <ButtonV2 variant="fill-light" color="primary" disabled>
+          </Button>
+          <Button variant="fill-light" color="primary" disabled>
             fill-light disabled
-          </ButtonV2>
-          <ButtonV2 variant="outline" color="primary">
+          </Button>
+          <Button variant="outline" color="primary">
             outline-light
-          </ButtonV2>
-          <ButtonV2 variant="outline" color="primary" disabled>
+          </Button>
+          <Button variant="outline" color="primary" disabled>
             outline-light disabled
-          </ButtonV2>
-          <ButtonV2 variant="ghost" color="primary">
+          </Button>
+          <Button variant="ghost" color="primary">
             ghost
-          </ButtonV2>
-          <ButtonV2 variant="ghost" color="primary" disabled>
+          </Button>
+          <Button variant="ghost" color="primary" disabled>
             ghost disabled
-          </ButtonV2>
+          </Button>
         </Group>
         <Group title="Secondary">
-          <ButtonV2 variant="fill" color="secondary">
+          <Button variant="fill" color="secondary">
             Fill
-          </ButtonV2>
-          <ButtonV2 variant="fill" color="secondary" disabled>
+          </Button>
+          <Button variant="fill" color="secondary" disabled>
             Fill disabled
-          </ButtonV2>
-          <ButtonV2 variant="fill-light" color="secondary">
+          </Button>
+          <Button variant="fill-light" color="secondary">
             fill-light
-          </ButtonV2>
-          <ButtonV2 variant="fill-light" color="secondary" disabled>
+          </Button>
+          <Button variant="fill-light" color="secondary" disabled>
             fill-light disabled
-          </ButtonV2>
-          <ButtonV2 variant="outline" color="secondary">
+          </Button>
+          <Button variant="outline" color="secondary">
             outline-light
-          </ButtonV2>
-          <ButtonV2 variant="outline" color="secondary" disabled>
+          </Button>
+          <Button variant="outline" color="secondary" disabled>
             outline-light disabled
-          </ButtonV2>
-          <ButtonV2 variant="ghost" color="secondary">
+          </Button>
+          <Button variant="ghost" color="secondary">
             ghost
-          </ButtonV2>
-          <ButtonV2 variant="ghost" color="secondary" disabled>
+          </Button>
+          <Button variant="ghost" color="secondary" disabled>
             ghost disabled
-          </ButtonV2>
+          </Button>
         </Group>
         <Group title="Success">
-          <ButtonV2 variant="fill" color="success">
+          <Button variant="fill" color="success">
             Fill
-          </ButtonV2>
-          <ButtonV2 variant="fill" color="success" disabled>
+          </Button>
+          <Button variant="fill" color="success" disabled>
             Fill disabled
-          </ButtonV2>
-          <ButtonV2 variant="fill-light" color="success">
+          </Button>
+          <Button variant="fill-light" color="success">
             fill-light
-          </ButtonV2>
-          <ButtonV2 variant="fill-light" color="success" disabled>
+          </Button>
+          <Button variant="fill-light" color="success" disabled>
             fill-light disabled
-          </ButtonV2>
-          <ButtonV2 variant="outline" color="success">
+          </Button>
+          <Button variant="outline" color="success">
             outline-light
-          </ButtonV2>
-          <ButtonV2 variant="outline" color="success" disabled>
+          </Button>
+          <Button variant="outline" color="success" disabled>
             outline-light disabled
-          </ButtonV2>
-          <ButtonV2 variant="ghost" color="success">
+          </Button>
+          <Button variant="ghost" color="success">
             ghost
-          </ButtonV2>
-          <ButtonV2 variant="ghost" color="success" disabled>
+          </Button>
+          <Button variant="ghost" color="success" disabled>
             ghost disabled
-          </ButtonV2>
+          </Button>
         </Group>
         <Group title="Destructive">
-          <ButtonV2 variant="fill" color="destructive">
+          <Button variant="fill" color="destructive">
             Fill
-          </ButtonV2>
-          <ButtonV2 variant="fill" color="destructive" disabled>
+          </Button>
+          <Button variant="fill" color="destructive" disabled>
             Fill disabled
-          </ButtonV2>
-          <ButtonV2 variant="fill-light" color="destructive">
+          </Button>
+          <Button variant="fill-light" color="destructive">
             fill-light
-          </ButtonV2>
-          <ButtonV2 variant="fill-light" color="destructive" disabled>
+          </Button>
+          <Button variant="fill-light" color="destructive" disabled>
             fill-light disabled
-          </ButtonV2>
-          <ButtonV2 variant="outline" color="destructive">
+          </Button>
+          <Button variant="outline" color="destructive">
             outline-light
-          </ButtonV2>
-          <ButtonV2 variant="outline" color="destructive" disabled>
+          </Button>
+          <Button variant="outline" color="destructive" disabled>
             outline-light disabled
-          </ButtonV2>
-          <ButtonV2 variant="ghost" color="destructive">
+          </Button>
+          <Button variant="ghost" color="destructive">
             ghost
-          </ButtonV2>
-          <ButtonV2 variant="ghost" color="destructive" disabled>
+          </Button>
+          <Button variant="ghost" color="destructive" disabled>
             ghost disabled
-          </ButtonV2>
+          </Button>
         </Group>
         <Group title="Default variants">
-          <ButtonV2>without variant</ButtonV2>
-          <ButtonV2 variant="fill">fill</ButtonV2>
-          <ButtonV2 variant="fill-light">fill-light</ButtonV2>
-          <ButtonV2 variant="outline">outline</ButtonV2>
-          <ButtonV2 variant="ghost">ghost</ButtonV2>
+          <Button>without variant</Button>
+          <Button variant="fill">fill</Button>
+          <Button variant="fill-light">fill-light</Button>
+          <Button variant="outline">outline</Button>
+          <Button variant="ghost">ghost</Button>
         </Group>
-        {/* <ButtonV2 disabled>Primary Disabled</ButtonV2> */}
+        {/* <Button disabled>Primary Disabled</Button> */}
 
         {/* <Button variant="outline">Outline</Button>
         <Button disabled variant="outline">
--- frontend/src/shared/ui/button/index.tsx
+++ frontend/src/shared/ui/button/index.tsx
@@ -1,22 +1,25 @@
-import * as React from 'react';
-import {Slot} from '@radix-ui/react-slot';
+import {forwardRef} from 'react';
 import {cva, type VariantProps} from 'class-variance-authority';
 
+import {Slot} from '@radix-ui/react-slot';
 import {cn} from '@shared/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring-primary disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  'inline-flex items-center justify-center text-btn-text gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring-primary disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-btn-text shadow hover:bg-btn-hovered-bg-primary',
-        secondary: 'bg-secondary text-btn-text shadow-sm hover:bg-btn-hovered-bg-secondary',
-        success: 'bg-success text-btn-text shadow-sm hover:bg-btn-hovered-bg-success',
-        destructive: 'bg-destructive text-btn-text shadow-sm hover:bg-btn-hovered-bg-destructive',
-        outline:
-          'border border-accent bg-background text-btn-text-outline shadow-sm hover:bg-accent',
-        ghost: 'text-btn-text-outline hover:bg-accent',
-        link: 'text-btn-text underline-offset-4 hover:underline',
+        fill: '',
+        'fill-light': 'hover:border-2 hover:border-primary-light',
+        outline: 'border border-accent',
+        ghost: 'text-text-primary',
+      },
+      color: {
+        default: 'bg-primary hover:bg-btn-hovered-bg-primary shadow',
+        primary: 'bg-primary hover:bg-btn-hovered-bg-primary shadow',
+        secondary: 'bg-secondary hover:bg-btn-hovered-bg-secondary shadow-sm',
+        success: 'bg-success shadow-sm hover:bg-btn-hovered-bg-success',
+        destructive: 'bg-destructive shadow-sm hover:bg-btn-hovered-bg-destructive',
       },
       size: {
         default: 'h-9 px-4 py-2',
@@ -25,23 +28,97 @@ const buttonVariants = cva(
         icon: 'h-9 w-9',
       },
     },
+    compoundVariants: [
+      {
+        variant: 'fill-light',
+        color: 'primary',
+        class: 'bg-primary-light text-primary hover:border-primary hover:bg-primary-light',
+      },
+      {
+        variant: 'fill-light',
+        color: 'secondary',
+        class: 'bg-secondary-light text-secondary hover:border-secondary hover:bg-secondary-light',
+      },
+      {
+        variant: 'fill-light',
+        color: 'success',
+        class: 'bg-success-light text-success hover:border-success hover:bg-success-light',
+      },
+      {
+        variant: 'fill-light',
+        color: 'destructive',
+        class:
+          'bg-destructive-light text-destructive hover:border-destructive hover:bg-destructive-light',
+      },
+      {
+        variant: 'outline',
+        color: 'primary',
+        class: 'text-primary bg-transparent hover:bg-accent shadow-none border-primary',
+      },
+      {
+        variant: 'outline',
+        color: 'secondary',
+        class: 'text-secondary bg-transparent hover:bg-accent shadow-none border-secondary',
+      },
+      {
+        variant: 'outline',
+        color: 'success',
+        class: 'text-success border-success bg-transparent hover:bg-accent shadow-none',
+      },
+      {
+        variant: 'outline',
+        color: 'destructive',
+        class: 'text-destructive border-destructive bg-transparent hover:bg-accent shadow-none',
+      },
+      {
+        variant: 'ghost',
+        class: 'bg-transparent hover:bg-accent shadow-none',
+      },
+      // default
+      {
+        variant: 'fill',
+        color: 'default',
+      },
+      {
+        variant: 'fill-light',
+        color: 'default',
+        class: 'bg-secondary hover:bg-btn-hovered-bg-secondary shadow-sm',
+      },
+      {
+        variant: 'outline',
+        color: 'default',
+        class: 'text-text-primary bg-transparent hover:bg-accent shadow-none',
+      },
+      {
+        variant: 'ghost',
+        color: 'default',
+        class: 'bg-transparent hover:bg-accent shadow-none',
+      },
+    ],
     defaultVariants: {
-      variant: 'default',
+      variant: 'fill',
+      color: 'default',
       size: 'default',
     },
   },
 );
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'color'>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({className, variant, size, asChild = false, ...props}, ref) => {
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({className, color, variant, size, asChild = false, ...props}, ref) => {
     const Comp = asChild ? Slot : 'button';
-    return <Comp className={cn(buttonVariants({variant, size, className}))} ref={ref} {...props} />;
+    return (
+      <Comp
+        className={cn(buttonVariants({variant, color, size, className}))}
+        ref={ref}
+        {...props}
+      />
+    );
   },
 );
 Button.displayName = 'Button';
