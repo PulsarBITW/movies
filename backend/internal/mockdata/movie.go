1fa16b1d9ab677d14cff9615d56562c468dd26a9
PulsarBITW <GordeyBITW@gmail.com>
2025-02-08 23:39:31 +0300

    update mock mediaList

--- backend/internal/mockdata/movie.go
+++ backend/internal/mockdata/movie.go
@@ -5,7 +5,7 @@ import "movies-backend/internal/models"
 var MovieList = []models.Movie{
 	{ID: 1, Title: "Inception", Genre: "Science Fiction", Company: "Warner Bros.", Year: 2010},
 	{ID: 2, Title: "Free Guy", Genre: "Comedy", Company: "20th Century Studios", Year: 2021},
-	{ID: 3, Title: "Nagi Film (Blue Lock)", Genre: "Anime", Company: "Bandai Namco", Year: 2024},
+	{ID: 3, Title: "Nagi's Film (Blue Lock)", Genre: "Anime", Company: "Bandai Namco", Year: 2024},
 	{ID: 4, Title: "Dune: Part One", Genre: "Science Fiction", Company: "Warner Bros.", Year: 2021},
 	{ID: 5, Title: "Deadpool", Genre: "Comedy", Company: "20th Century Fox", Year: 2016},
 }
