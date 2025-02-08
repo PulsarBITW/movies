c3a60e623cef0c1302ef47d8cc4cedcfc4f66712
PulsarBITW <GordeyBITW@gmail.com>
2025-02-08 23:40:24 +0300

    fix

--- backend/internal/models/movie.go
+++ backend/internal/models/movie.go
@@ -1,9 +1,9 @@
 package models
 
 type Movie struct {
-	ID      int    `json:"id"`      // Уникальный идентификатор
-	Title   string `json:"title"`   // Название фильма
-	Genre   string `json:"genre"`   // Жанр
-	Company string `json:"company"` // Компания издатель
-	Year    int    `json:"year"`    // Год выпуска
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Genre   string `json:"genre"`
+	Company string `json:"company"`
+	Year    int    `json:"year"`
 }
